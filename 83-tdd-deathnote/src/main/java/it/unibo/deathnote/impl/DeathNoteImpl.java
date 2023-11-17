97095d252ad29679054ef0abd27f124d5fd09851
Marcatelli Francesco <francesco.marcatelli@studio.unibo.it>
2023-11-17 16:11:39 +0100

    Implemented DeathNoteImpl class

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -1,12 +1,35 @@
 package it.unibo.deathnote.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import it.unibo.deathnote.api.DeathNote;
 
 public class DeathNoteImpl implements DeathNote{
 
+    private final String DEFAULT_CAUSE = "heart attack";
+
+    private class Death {
+        private String cause;
+        private String details;
+
+        private void setCause(String cause){
+            this.cause = DEFAULT_CAUSE;
+        }
+
+        private void setDetails(String details) {
+            this.details = details;
+        }
+    }
+
+    private static final long MS_LIMIT_TO_WRITE_CAUSE = 40;
+    private static final long MS_LIMIT_TO_WRITE_DETAILS = 6040;
+
+    private final Map<String, Death> map = new HashMap<>();
+    private final TimerImpl timer = new TimerImpl();
     private String lastName;
+
 
     @Override
     public String getRule(int ruleNumber) {
@@ -18,37 +41,60 @@ public class DeathNoteImpl implements DeathNote{
 
     @Override
     public void writeName(final String name) {
-        
+        Objects.requireNonNull(name);
+        lastName = name;
+        map.put(lastName, new Death());
+        timer.reset();
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+        if (lastName == null || cause == null) {
+            throw new IllegalArgumentException("There's no valid name or cause");
+        }
+        final long time = timer.getTime();
+        if (time > MS_LIMIT_TO_WRITE_CAUSE) {
+            return false;
+        }
+        map.get(lastName).setCause(cause);
+        return true;
     }
 
     @Override
     public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+        if (lastName == null || details == null) {
+            throw new IllegalArgumentException("There's no valid name or details");
+        }
+        final long time = timer.stop();
+        if (time > MS_LIMIT_TO_WRITE_DETAILS) {
+            return false;
+        }
+        map.get(lastName).setDetails(details);
+        return true;
+    }
+
+    private Death equalDeath(String name) {
+        final Death death = map.get(name);
+
+        if (death == null) {
+            throw new IllegalArgumentException("The name" + name + "is not on the DeathNote");
+        }
+        return death;
     }
 
     @Override
     public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+        return equalDeath(name).cause;
     }
 
     @Override
     public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+        return equalDeath(name).details;
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+        return map.containsKey(name);
     }
     
 }
